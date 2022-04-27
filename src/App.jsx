8786996bb4320e7f54375b620dfbd2a18a3fc786
Barox Phan <pqb.barox@gmail.com>
2022-04-27 16:50:39 +0700

    design trello #1 (1:10:31:)

---
+++ src/App.jsx
@@ -0,0 +1,138 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="trello-barox-master">
+      <nav className="navbar app">App Navbar</nav>
+      <nav className="navbar board">Board Navbar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+            <li>some task</li>
+
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+        <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+            <div className="column">
+          <header>This is header</header>
+          <ul>
+            <li>
+              <img src={'https://source.unsplash.com/random'} alt="" />
+              This is a task
+            </li>
+            <li>some task</li>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+          </ul>
+          <footer>
+            Add another task
+          </footer>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
